b611cee1457b25dc2ccd71a0fb3ebebe9c05f1d8
Stella Nthenya <stellaalogi@gmail.com>
2023-05-25 22:09:54 +0300

    rendered comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import video from "../data/video.js";
 import Item from "./Item.jsx";
 
+
 function App() {
   console.log("Here's your data:", video);
 
   return (
     <div className="App">
-      <Item video={video}/>      
+      <Item video={video}/>
     </div>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,3 +1,5 @@
+import { AiTwotoneLike, AiFillDislike } from 'react-icons/ai';
+
 function Item({video}){
     return(
         <>
@@ -11,6 +13,19 @@ function Item({video}){
             />
             <h2>{video.title}</h2>
             <p>{video.views} | Uploaded {video.createdAt}</p>
+            <h2>{video.comments.map((comment)=>{
+                return(
+                    <div>
+                        <h3>{comment.user}</h3>
+                        <h5>{comment.user}</h5>
+                    </div>
+                )
+            })}
+            </h2>
+
+            <button><AiTwotoneLike />Like</button>
+            <button><AiFillDislike/>Dislike</button>
+
         </>
     )
 }
